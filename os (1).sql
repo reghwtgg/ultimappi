f3bc14eab42d02e9254fac80f8f9d3f41a7da8a2
Anderson Jacques <55748133+jacquesgabi@users.noreply.github.com>
2019-12-06 08:54:28 -0300

    Banco

---
+++ os (1).sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 06-Dez-2019 às 11:54
+-- Versão do servidor: 5.7.21
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `os`
+--
+CREATE DATABASE IF NOT EXISTS `os` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `os`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aparelho`
+--
+
+DROP TABLE IF EXISTS `aparelho`;
+CREATE TABLE IF NOT EXISTS `aparelho` (
+  `id_aparelho` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(10) DEFAULT NULL,
+  `modelo` varchar(50) DEFAULT NULL,
+  `marca` varchar(50) DEFAULT NULL,
+  `imei` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_aparelho`),
+  KEY `id_cliente` (`id_cliente`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `aparelho`
+--
+
+INSERT INTO `aparelho` (`id_aparelho`, `id_cliente`, `modelo`, `marca`, `imei`) VALUES
+(1, 1, 'iphone 6s plus', 'apple', '1651514851');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `telefone` varchar(50) DEFAULT NULL,
+  `documento` varchar(25) DEFAULT NULL,
+  `endereco` varchar(200) DEFAULT NULL,
+  `bairro` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `os`
+--
+
+DROP TABLE IF EXISTS `os`;
+CREATE TABLE IF NOT EXISTS `os` (
+  `id_os` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(10) DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  `estado` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id_os`),
+  KEY `id_cliente` (`id_cliente`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
